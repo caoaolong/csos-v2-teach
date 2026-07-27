06536e20c22d3fc46e6bce150c0d60dd5eea0628
caoaolong <eason105cc@163.com>
2026-07-28 00:18:44 +0800

    003.UEFI初探

--- qemu.bat
+++ qemu.bat
@@ -1,5 +1,8 @@
+cp -f edk2\Build\MdeModule\DEBUG_VS2026\X64\HelloWorld.efi qemu/hda-contents/
+
 qemu-system-x86_64 ^
  -bios edk2\Build\OvmfX64\NOOPT_VS2026\FV\OVMF.fd ^
+ -drive format=raw,file=fat:rw:qemu/hda-contents ^
  -net none ^
  -debugcon file:debug.log ^
  -global isa-debugcon.iobase=0x402
